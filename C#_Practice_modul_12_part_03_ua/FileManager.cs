f6e95ae824e4bdc2d6ec98134004cad009a0fb90
RedOniLL <Gwnbtttt@gmail.com>
2024-05-12 11:19:34 +0300

    Add project files.

---
+++ C#_Practice_modul_12_part_03_ua/FileManager.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace C__Practice_modul_12_part_03_ua
+{
+    public static class FileManager
+    {
+        public static bool CopyFile(string sourcePath, string destinationPath)
+        {
+            try
+            {
+                if (!File.Exists(sourcePath))
+                {
+                    Console.WriteLine("Error: File not found.");
+                    return false;
+                }
+
+                File.Copy(sourcePath, Path.Combine(destinationPath, Path.GetFileName(sourcePath)), true);
+
+                if (File.Exists(Path.Combine(destinationPath, Path.GetFileName(sourcePath))))
+                {
+                    Console.WriteLine("File copied successfully.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Error: File not copied.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+        }
+
+        public static bool MoveFile(string sourcePath, string destinationPath)
+        {
+            try
+            {
+                if (!File.Exists(sourcePath))
+                {
+                    Console.WriteLine("Error: File not found.");
+                    return false;
+                }
+
+                File.Move(sourcePath, Path.Combine(destinationPath, Path.GetFileName(sourcePath)));
+
+                if (File.Exists(Path.Combine(destinationPath, Path.GetFileName(sourcePath))))
+                {
+                    Console.WriteLine("File moved successfully.");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Error: File not moved.");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+        }
+
+        public static bool CopyFolder(string sourcePath, string destinationPath)
+        {
+            try
+            {
+                if (!Directory.Exists(sourcePath))
+                {
+                    Console.WriteLine("Error: Source folder not found.");
+                    return false;
+                }
+
+                Directory.CreateDirectory(destinationPath);
+
+                foreach (string file in Directory.GetFiles(sourcePath))
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destFile = Path.Combine(destinationPath, fileName);
+                    File.Copy(file, destFile, true);
+                }
+
+                foreach (string subDir in Directory.GetDirectories(sourcePath))
+                {
+                    string dirName = new DirectoryInfo(subDir).Name;
+                    string destDir = Path.Combine(destinationPath, dirName);
+                    CopyFolder(subDir, destDir);
+                }
+
+                Console.WriteLine("Folder copied successfully.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+        }
+
+        public static bool MoveFolder(string sourcePath, string destinationPath)
+        {
+            try
+            {
+                if (!Directory.Exists(sourcePath))
+                {
+                    Console.WriteLine("Error: Source folder not found.");
+                    return false;
+                }
+
+                if (!Directory.Exists(destinationPath))
+                {
+                    Directory.CreateDirectory(destinationPath);
+                }
+
+                foreach (string file in Directory.GetFiles(sourcePath))
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destFile = Path.Combine(destinationPath, fileName);
+                    File.Move(file, destFile);
+                }
+
+                foreach (string subDir in Directory.GetDirectories(sourcePath))
+                {
+                    string dirName = new DirectoryInfo(subDir).Name;
+                    string destDir = Path.Combine(destinationPath, dirName);
+                    MoveFolder(subDir, destDir);
+                }
+
+                Directory.Delete(sourcePath);
+
+                Console.WriteLine("Folder moved successfully.");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
---
+++ C#_Practice_modul_12_part_03_ua/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace C__Practice_modul_12_part_03_ua
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Enter the path to the original file:");
+                    string sourcePath1 = Console.ReadLine();
+
+                    Console.WriteLine("Enter the path where the file should be copied:");
+                    string destinationPath1 = Console.ReadLine();
+
+                    FileManager.CopyFile(sourcePath1, destinationPath1);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the path to the original file:");
+                    string sourcePath2 = Console.ReadLine();
+
+                    Console.WriteLine("Enter the path where the file should be moved:");
+                    string destinationPath2 = Console.ReadLine();
+
+                    FileManager.MoveFile(sourcePath2, destinationPath2);
+                    break;
+                case 3:
+                    Console.WriteLine("Enter the path to the source folder:");
+                    string sourcePath3 = Console.ReadLine();
+
+                    Console.WriteLine("Enter the path where the folder should be copied:");
+                    string destinationPath3 = Console.ReadLine();
+
+                    FileManager.CopyFolder(sourcePath3, destinationPath3);
+                    break;
+                case 5:
+                    Console.WriteLine("Enter the path to the source folder:");
+                    string sourcePath5 = Console.ReadLine();
+
+                    Console.WriteLine("Enter the path where the folder should be moved:");
+                    string destinationPath5 = Console.ReadLine();
+
+                    FileManager.MoveFolder(sourcePath5, destinationPath5);
+                    break;
+            }
+        }
+    }
+}
